b244acff333290576597c1f96ee602ba1e7d44e7
Ivan Velev <vanecat@gmail.com>
2018-01-12 01:26:49 -0800

    SELECT Dynamic Resizing Tests
    Removed the special CSS from the tests, because it was affecting negatively (in unexpected ways), the way the SELECT javascript-drive height resizing/adjustment works.
    I need to later understand how some of those CSS properties ("overflow", "min-height" on the SELECT element) affect JS in isolation of each other.
    
    Added a JS test for whether the SELECT Fancy Widget is enabled (e.g. for most mobile device browsers).  The assumption is that if then default OPTION elements are not available for selection/clicking (i.e. Fancy Widget is handling the selection), their CSS (inner) height (i.e. height - margin/border) will be 0.  I create a SELECT element in a controlled manner on the fly and measure its OPTION elements heights. Because it's a controlled manner, I can be sure to make the margin/border/padding to be zero (0).
    NOTE: The modern cool Internet "hot" tools (e.g. Modernizr) don't offer such tests. So I had to make my own.  It is based on assumptions, but it works for my special case/scenario.

--- select-resize-dynamic/select-resize-dynamic.js
+++ select-resize-dynamic/select-resize-dynamic.js
@@ -3,7 +3,6 @@
  */
 
 function resizeSelectBasedOnOptionsHeight($) {
-    //return;
     var h = 0, maxW = 0;
     var dims = [];
     $('select option').each(function(i,o){
@@ -21,4 +20,18 @@ function resizeSelectBasedOnOptionsHeight($) {
     }
 
     $('select').height( h  ).width( maxW ).removeAttr('size');
+}
+
+function testSelectFancyWidgetEnabled($) {
+    var randId = 'testSelect' + Math.ceil(Math.random() * 100000);
+    var $body = $('body');
+    $body.append('<select style="position: absolute; z-index: 1234; top: -999px; left: -999px;" id="' + randId + '" multiple><option>1</option><option>2</option></select>');
+
+    var $options = $('#'+randId + ' option');
+    var optionHeight = $($options[0]).height();
+
+    $body.append("<div>Test Option height: "+optionHeight+", Test Option Outer Height: "+$($options[0]).outerHeight()+"</div>")
+
+    // if height is 0, then default options are not available for selection/clicking (i.e. Fancy Widget is handling the selection)
+    return !!optionHeight;
 }
