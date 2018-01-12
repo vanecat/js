2800a2e67a12cfc16226eebc59013b25fc3e75c4
Ivan Velev <vanecat@gmail.com>
2018-01-12 01:33:23 -0800

    SELECT Dynamic Resizing Tests
    Add more notes to JS test itself.
    Increased the top/left "off-screen" properties of the test SELECT element to rule out fringe cases where 999px are not enough to place an absolutely positioned element out of view.
     Decreased z-index for the same reason: to decrease the slim likelihood of it showing up on top of other element.
     Added "!important" to all styles.
    
     We have "fortified" this test: it can withstand the "toughest" conditions out there and never make itself known to a user.

--- select-resize-dynamic/select-resize-dynamic.js
+++ select-resize-dynamic/select-resize-dynamic.js
@@ -22,10 +22,19 @@ function resizeSelectBasedOnOptionsHeight($) {
     $('select').height( h  ).width( maxW ).removeAttr('size');
 }
 
+// A test for whether the SELECT Fancy Widget is enabled (e.g. for most mobile device browsers).
+// The assumption is that if then default OPTION elements are not available for selection/clicking
+// (i.e. Fancy Widget is handling the selection), their CSS (inner) height (i.e. height - margin/border) will be 0.
+// The test uses a SELECT element created in a controlled/programmatic manner on the fly and measures
+// the OPTION elements heights. Because it's a controlled manner, I can be sure to make the margin/border/padding to be zero (0).
 function testSelectFancyWidgetEnabled($) {
     var randId = 'testSelect' + Math.ceil(Math.random() * 100000);
     var $body = $('body');
-    $body.append('<select style="position: absolute; z-index: 1234; top: -999px; left: -999px;" id="' + randId + '" multiple><option>1</option><option>2</option></select>');
+
+    // The test SELECT element styles need to
+    //   to set border/margin/padding to 0 to measure width in "isolation"
+    //   to be out of user's view (hence positioned absolute and top/left out of screen)
+    $body.append('<select style="margin: 0!important; padding: 0!important; position: absolute!important; z-index: 1!important; top: -9999px!important; left: -9999px!important;" id="' + randId + '" multiple><option>1</option><option>2</option></select>');
 
     var $options = $('#'+randId + ' option');
     var optionHeight = $($options[0]).height();
